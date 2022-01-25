f46aa5ac3edfe7f13cf2a44a84111bd2e0e3c43a
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2022-01-25 23:34:02 +0800

    Setup post entity

--- src/main/java/com/adamscript/tomatetoapi/models/entities/Post.java
+++ src/main/java/com/adamscript/tomatetoapi/models/entities/Post.java
@@ -1,4 +1,51 @@
 package com.adamscript.tomatetoapi.models.entities;
 
-public class Post {
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "post_tbl")
+public class Post implements Serializable {
+
+    @Id
+    private String postId;
+
+    @OneToOne
+    @NotNull
+    private User userId;
+
+    private Instant date;
+
+    private String status;
+
+    @OneToOne
+    private Story storyId;
+
+    @OneToOne
+    private Rating ratingId;
+
+    @OneToMany
+    private ArrayList<Comment> comments = new ArrayList<>();
+
+    @OneToMany
+    private ArrayList<User> likes = new ArrayList<>();
+
+    public Post(User userId, String status, Story storyId, Rating ratingId){
+        this.postId = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.date = Instant.now();
+        this.status = status;
+        this.storyId = storyId;
+        this.ratingId = ratingId;
+    }
+
 }
