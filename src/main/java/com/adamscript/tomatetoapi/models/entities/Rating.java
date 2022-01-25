0967aa4eb602ae090fc2495b546d8053c75063a7
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2022-01-26 00:00:16 +0800

    Setup rating entity

--- src/main/java/com/adamscript/tomatetoapi/models/entities/Rating.java
+++ src/main/java/com/adamscript/tomatetoapi/models/entities/Rating.java
@@ -1,4 +1,33 @@
 package com.adamscript.tomatetoapi.models.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "rating_tbl")
 public class Rating {
+
+    @Id
+    private String ratingId;
+
+    @OneToOne
+    private Library libraryId;
+
+    @OneToOne
+    private User userId;
+
+    private Integer rating;
+
+    public Rating(Library libraryId, User userId, Integer rating){
+        this.ratingId = UUID.randomUUID().toString();
+        this.libraryId = libraryId;
+        this.userId = userId;
+        this.rating = rating;
+    }
+
 }
