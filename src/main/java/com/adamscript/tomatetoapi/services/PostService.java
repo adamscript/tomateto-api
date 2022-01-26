b0d7b484b2314687ded790c9f4e58e9270f2d24c
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2022-01-27 02:06:21 +0800

    Setup post list by id service

--- src/main/java/com/adamscript/tomatetoapi/services/PostService.java
+++ src/main/java/com/adamscript/tomatetoapi/services/PostService.java
@@ -1,4 +1,36 @@
 package com.adamscript.tomatetoapi.services;
 
+import com.adamscript.tomatetoapi.models.entities.Post;
+import com.adamscript.tomatetoapi.models.repos.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class PostService {
+
+    @Autowired
+    private PostRepo postRepo;
+
+    //fetch post content data
+    public Post list(Long id){
+        return postRepo.findById(id).get();
+    };
+
+    //create/edit post
+    public Post insert(Post product){
+        return postRepo.save(product);
+    }
+
+    //liking a post
+
+    //disliking a post
+
+    //deleting a post
+    public void delete(Long id){
+        postRepo.deleteById(id);
+    }
+
 }
