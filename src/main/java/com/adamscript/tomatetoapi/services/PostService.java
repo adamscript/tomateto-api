2b39d61fbf2a3efe89ac3bc411741997f5efa0e0
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2022-01-27 09:07:31 +0800

    Setup post service

--- src/main/java/com/adamscript/tomatetoapi/services/PostService.java
+++ src/main/java/com/adamscript/tomatetoapi/services/PostService.java
@@ -1,6 +1,8 @@
 package com.adamscript.tomatetoapi.services;
 
 import com.adamscript.tomatetoapi.models.entities.Post;
+import com.adamscript.tomatetoapi.models.entities.PostLike;
+import com.adamscript.tomatetoapi.models.repos.PostLikeRepo;
 import com.adamscript.tomatetoapi.models.repos.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,22 +16,31 @@ public class PostService {
     @Autowired
     private PostRepo postRepo;
 
+    @Autowired
+    private PostLikeRepo postLikeRepo;
+
     //fetch post content data
-    public Post list(Long id){
+    public Post list(long id){
         return postRepo.findById(id).get();
     };
 
     //create/edit post
-    public Post insert(Post product){
-        return postRepo.save(product);
+    public Post insert(Post post){
+        return postRepo.save(post);
     }
 
     //liking a post
+    public PostLike like(PostLike postLike){
+        return postLikeRepo.save(postLike);
+    }
 
     //disliking a post
+    public void dislike(long id){
+        postLikeRepo.deleteById(id);
+    }
 
     //deleting a post
-    public void delete(Long id){
+    public void delete(long id){
         postRepo.deleteById(id);
     }
 
