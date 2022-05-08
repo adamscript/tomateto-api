b6e9e30a0118020acb4147831e9f8374917f0b12
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-08 22:22:12 +0800

    setup userRepository constructor

--- src/main/java/com/adamscript/tomatetoapi/services/UserService.java
+++ src/main/java/com/adamscript/tomatetoapi/services/UserService.java
@@ -4,6 +4,7 @@ import com.adamscript.tomatetoapi.models.entities.Post;
 import com.adamscript.tomatetoapi.models.entities.User;
 import com.adamscript.tomatetoapi.models.repos.PostRepository;
 import com.adamscript.tomatetoapi.models.repos.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,10 @@ import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     //fetch post content data
     public User list(long id){
