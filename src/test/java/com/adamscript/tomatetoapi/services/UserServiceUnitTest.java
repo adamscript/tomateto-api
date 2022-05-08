c78cc5b7f3b2d529c687f2ebc2ac8e99ac026bf2
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-08 22:13:42 +0800

    created user service unit test

---
+++ src/test/java/com/adamscript/tomatetoapi/services/UserServiceUnitTest.java
@@ -0,0 +1,12 @@
+package com.adamscript.tomatetoapi.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceUnitTest {
+
+    @Test
+    void list() {
+    }
+}
