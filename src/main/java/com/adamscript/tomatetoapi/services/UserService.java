2299327476baea7f1c650e456d9dd9d061d00506
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-13 10:54:01 +0800

    setup user service error handling when user not found

--- src/main/java/com/adamscript/tomatetoapi/services/UserService.java
+++ src/main/java/com/adamscript/tomatetoapi/services/UserService.java
@@ -2,13 +2,13 @@ package com.adamscript.tomatetoapi.services;
 
 import com.adamscript.tomatetoapi.models.entities.Post;
 import com.adamscript.tomatetoapi.models.entities.User;
-import com.adamscript.tomatetoapi.models.repos.PostRepository;
 import com.adamscript.tomatetoapi.models.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -18,8 +18,14 @@ public class UserService {
     private final UserRepository userRepository;
 
     //fetch post content data//
-    public User list(long id){
-        return userRepository.findById(id).get();
-    };
+    public Optional<User> list(long id) {
+        Optional<User> user = userRepository.findById(id);
 
+        if (user.isPresent()) {
+            return Optional.of(user.get());
+        } else {
+            System.out.println("User not found");
+            return null;
+        }
+    }
 }
