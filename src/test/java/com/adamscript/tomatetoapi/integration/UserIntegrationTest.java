9c35b84b38c363057f9839c6de42d666232c8b7f
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-16 20:54:16 +0800

    setup insert user test cases

--- src/test/java/com/adamscript/tomatetoapi/integration/UserIntegrationTest.java
+++ src/test/java/com/adamscript/tomatetoapi/integration/UserIntegrationTest.java
@@ -2,13 +2,19 @@ package com.adamscript.tomatetoapi.integration;
 
 import com.adamscript.tomatetoapi.models.entities.User;
 import com.adamscript.tomatetoapi.models.repos.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +25,9 @@ public class UserIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private UserRepository userRepository;
@@ -33,6 +42,25 @@ public class UserIntegrationTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.username").value(user.getUsername()));
+
+    }
+
+    @Test
+    void insertNewUser() throws Exception {
+        User user = new User();
+        user.setUsername("eyesocketdisc");
+        user.setDisplayName("EyeSocketDisc");
+
+        mockMvc.perform(post("/api/user")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(user.getUsername()));
+    }
+
+    @Test
+    void editUser() throws Exception {
 
     }
 
