2eb07e916b87c4ec5d17857b6b3dc9d0441d8363
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-16 20:56:51 +0800

    added findByUsername inferred query

--- src/main/java/com/adamscript/tomatetoapi/models/repos/UserRepository.java
+++ src/main/java/com/adamscript/tomatetoapi/models/repos/UserRepository.java
@@ -3,5 +3,10 @@ package com.adamscript.tomatetoapi.models.repos;
 import com.adamscript.tomatetoapi.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
 }
