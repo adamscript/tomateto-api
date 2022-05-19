dbc3bd5ed7ad4f526651aa540c6e765590f9e14e
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-20 01:36:57 +0800

    implemented unfollow and edit for User, also setup error handling for bad requests

--- src/main/java/com/adamscript/tomatetoapi/services/UserService.java
+++ src/main/java/com/adamscript/tomatetoapi/services/UserService.java
@@ -1,58 +1,136 @@
 package com.adamscript.tomatetoapi.services;
 
+import com.adamscript.tomatetoapi.helpers.handler.Response;
+import com.adamscript.tomatetoapi.helpers.service.ServiceStatus;
 import com.adamscript.tomatetoapi.models.entities.User;
 import com.adamscript.tomatetoapi.models.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
 
     //fetch user information//
-    public Optional<User> list(long id) {
+    public Response list(long id) {
         Optional<User> user = userRepository.findById(id);
 
         if (user.isPresent()) {
-            return Optional.of(user.get());
-        } else {
-            System.out.println("User not found");
-            return null;
+            return new Response(user, ServiceStatus.SUCCESS);
+        }
+        else if (user.isEmpty()){
+            return new Response(null, ServiceStatus.USER_NOT_FOUND);
+        }
+        else{
+            return new Response(null, ServiceStatus.ERROR);
         }
     }
 
     //register new user
-    public Optional<User> insert(User user){
+    public Response insert(User user){
         Optional<User> insertedUser = userRepository.findByUsername(user.getUsername());
 
-        if (insertedUser.isEmpty()) {
-            return Optional.of(userRepository.save(user));
+        if(user.getUsername() == null){
+            return new Response(null, ServiceStatus.USERNAME_EMPTY);
+        }
+        else if(user.getDisplayName() == null){
+            return new Response(null, ServiceStatus.DISPLAYNAME_EMPTY);
+        }
+        else if (insertedUser.isPresent()){
+            return new Response(null, ServiceStatus.USERNAME_ALREADY_EXISTS);
+        }
+        else if (insertedUser.isEmpty()) {
+            User savedUser = userRepository.save(user);
+            return new Response(savedUser, ServiceStatus.SUCCESS);
+        }
+        else{
+            return new Response(null, ServiceStatus.ERROR);
+        }
+    }
+
+    public Response edit(User user){
+        Optional<User> insertedUser = userRepository.findById(user.getId());
+
+        if(user.getUsername() == null){
+            return new Response(null, ServiceStatus.USERNAME_EMPTY);
+        }
+        else if(user.getDisplayName() == null){
+            return new Response(null, ServiceStatus.DISPLAYNAME_EMPTY);
+        }
+        else if (insertedUser.isEmpty()){
+            return new Response(null, ServiceStatus.USER_DOES_NOT_EXISTS);
+        }
+        else if (insertedUser.isPresent()) {
+            //maintain user info before saving the update
+            user.setDate(insertedUser.get().getDate());
+            user.setFollowCount(insertedUser.get().getFollowCount());
+            user.setFollow(insertedUser.get().getFollow());
+            user.setFollowersCount(insertedUser.get().getFollowersCount());
+            user.setFollowers(insertedUser.get().getFollowers());
+            user.setPostsCount(insertedUser.get().getPostsCount());
+            user.setPost(insertedUser.get().getPost());
+            user.setLikedPosts(insertedUser.get().getLikedPosts());
+            user.setComment(insertedUser.get().getComment());
+            user.setLikedComments(insertedUser.get().getLikedComments());
+
+            User savedUser = userRepository.save(user);
+            return new Response(savedUser, ServiceStatus.SUCCESS);
         }
         else {
-            System.out.println("User already registered!");
-            return null;
+            return new Response(null, ServiceStatus.ERROR);
         }
     }
 
     //follow a user
-    public Optional<User> follow(Long userFollowingId, Long userFollowedId) {
+    public Response follow(Long userFollowingId, Long userFollowedId) {
         //validate if user exists
         Optional<User> userFollowing = userRepository.findById(userFollowingId);
         Optional<User> userFollowed = userRepository.findById(userFollowedId);
 
-        if(userFollowingId != userFollowedId && userFollowed.isPresent() && userFollowing.isPresent()){
+        List<User> userFollow = userRepository.findFollow(userFollowingId, userFollowed);
+
+        if(userFollowingId == userFollowedId){
+            return new Response(null, ServiceStatus.FOLLOW_YOURSELF);
+        }
+        else if (userFollowed.isEmpty()){
+            return new Response(null, ServiceStatus.FOLLOW_DOES_NOT_EXIST);
+        }
+        else if (userFollowing.isEmpty()){
+            return new Response(null, ServiceStatus.FOLLOWER_DOES_NOT_EXIST);
+        }
+        else if(!userFollow.isEmpty()){
+            return new Response(null, ServiceStatus.FOLLOWED_ALREADY);
+        }
+        else if(userFollowingId != userFollowedId && userFollowed.isPresent() && userFollowing.isPresent()){
             userRepository.followUser(userFollowingId, userFollowedId);
-            return userRepository.findById(userFollowingId);
+            return new Response(null, ServiceStatus.SUCCESS);
         }
         else{
-            System.out.println("User does not exists or you are trying to follow yourself");
-            return null;
+            return new Response(null, ServiceStatus.ERROR);
+        }
+    }
+
+    //unfollow a user
+    public Response unfollow(Long userFollowingId, Long userFollowedId) {
+        Optional<User> userFollowed = userRepository.findById(userFollowedId);
+
+        List<User> userFollow = userRepository.findFollow(userFollowingId, userFollowed);
+
+        if(userFollow.isEmpty()){
+            return new Response(null, ServiceStatus.NOT_FOLLOWED);
+        }
+        else if(!userFollow.isEmpty()){
+            userRepository.unfollowUser(userFollowingId, userFollowedId);
+            return new Response(null, ServiceStatus.SUCCESS);
+        }
+        else{
+            return new Response(null, ServiceStatus.ERROR);
         }
     }
 }
