4412026e53d9a5d2635674e9b570f444815597f7
Adam Darmawan <ajiaditya27@gmail.com>
2022-05-22 01:03:15 +0800

    added additional error codes and messages for Post and Comment

--- src/main/java/com/adamscript/tomatetoapi/helpers/service/ServiceStatus.java
+++ src/main/java/com/adamscript/tomatetoapi/helpers/service/ServiceStatus.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 @Getter
 public enum ServiceStatus {
 
+    //General
     SUCCESS(0, "Success!"),
+
+    //User
     USER_NOT_FOUND(100, "User not found"),
-    USERNAME_ALREADY_EXISTS(101, "Username already exists"),
-    USER_DOES_NOT_EXISTS(102, "User does not exist (Invalid User ID)"),
+    USERNAME_ALREADY_EXIST(101, "Username already exists"),
+    USER_DOES_NOT_EXIST(102, "User does not exist (Invalid User ID)"),
     USERNAME_EMPTY(103, "Username can't be empty"),
     DISPLAYNAME_EMPTY(104, "Name can't be empty"),
     FOLLOW_YOURSELF(105, "You can't follow yourself"),
@@ -16,6 +19,24 @@ public enum ServiceStatus {
     FOLLOWER_DOES_NOT_EXIST(107, "You can't follow if you does not exist"),
     FOLLOWED_ALREADY(108, "User already followed"),
     NOT_FOLLOWED(109, "User not followed"),
+
+    //Post
+    POST_NOT_FOUND(200, "Post not found"),
+    POST_DOES_NOT_EXIST(201, "Post does not exist (Invalid Post ID"),
+    POST_USER_EMPTY(202, "Post's user can't be empty"),
+    POST_CONTENT_EMPTY(203, "Post's content can't be empty"),
+    POST_LIKED_ALREADY(204, "Post already liked"),
+    POST_NOT_LIKED(205, "Post not liked"),
+
+    //Comment
+    COMMENT_NOT_FOUND(300, "Comment not found"),
+    COMMENT_DOES_NOT_EXIST(301, "Comment does not exist (Invalid Comment ID"),
+    COMMENT_USER_EMPTY(302, "Comment's user can't be empty"),
+    COMMENT_CONTENT_EMPTY(303, "Comment's content can't be empty"),
+    COMMENT_LIKED_ALREADY(304, "Comment already liked"),
+    COMMENT_NOT_LIKED(305, "Comment not liked"),
+
+    //Unknown
     ERROR(400, "An unknown error occurred");
 
     private final int code;
