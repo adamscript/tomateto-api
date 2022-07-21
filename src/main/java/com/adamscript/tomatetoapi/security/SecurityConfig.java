dba8d62e2058a3392ac613bb75a7befafd2708ca
Adam Darmawan <ajiaditya27@gmail.com>
2022-07-21 13:16:49 +0800

    cors fix

--- src/main/java/com/adamscript/tomatetoapi/security/SecurityConfig.java
+++ src/main/java/com/adamscript/tomatetoapi/security/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://tomateto.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://tomateto.com"));
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
